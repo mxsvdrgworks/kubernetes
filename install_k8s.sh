2fa2ee9fd517ca6a979899f7e14b617f80be0e15
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-30 17:14:34 +0200

    kubectl+minikube installation

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This installation is working on the Docker driver
 #Download the latest release
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
@@ -15,3 +16,11 @@ kubectl version --client
 #Minikube installation
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+#Checking minikube version
+minikube version
+#Adding minikube alias to the kubectl
+alias kubectl="minikube kubectl --"
+
+#Starting simple cluster
+minikube start
+kubectl get nodes
