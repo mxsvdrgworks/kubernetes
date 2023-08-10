901359b8694da8afb4f36e6ed41f8a7e5dc11ef7
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-08-10 15:25:56 +0200

    Delete check_scheduler_is_assigned.sh

--- check_scheduler_is_assigned.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl get pods --namespace=kube-system | grep db-custom-scheduler
-#Check assigning to a scheduler
-kubectl get events -o wide
-kubectl logs db-custom-scheduler --namespace=kube-system > /home/ubuntu/k8s_checks/db_scheduler.log
